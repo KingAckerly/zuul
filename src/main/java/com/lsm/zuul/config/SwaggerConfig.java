2bd68c5d02496bb4aa1c400f618ed87f1266e910
17727921123 <caiang.666>
2020-10-20 09:42:02 +0800

    springcloud整合swagger

--- src/main/java/com/lsm/zuul/config/SwaggerConfig.java
+++ src/main/java/com/lsm/zuul/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.lsm.zuul.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,11 +18,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a> 
  * 2019/05/04 12:44
  */
-@EnableSwagger2
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
-    @Bean
+    @Bean(value = "zuulApi")
     @Order(value = 1)
     public Docket groupRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
